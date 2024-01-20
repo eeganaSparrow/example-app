04abd3cde97ca343ccd4db76100cc53dd85453a6
sparrow <66nozorome@gmail.com>
2024-01-20 09:29:59 +0900

    TweetServiceTestのタイポを修正

--- tests/Unit/Services/TweetServiceTest.php
+++ tests/Unit/Services/TweetServiceTest.php
@@ -16,14 +16,14 @@ class TweetServiceTest extends TestCase
      */
     public function test_check_own_tweet(): void
     {
-        $imageManager = Mockey::mock(ImageManagerInterface::class);
-        $tweetService = new TweetService($imageManager);
-
         $mock = Mockery::mock('alias:App\Models\Tweet');
         $mock->shouldReceive('where->first')->andReturn((object)[
             'id' => 1,
             'user_id' => 1,
         ]);
+
+        $imageManager = Mockery::mock(ImageManagerInterface::class);
+        $tweetService = new TweetService($imageManager);
 
         $result = $tweetService->checkOwnTweet(1,1);
         $this->assertTrue($result);
